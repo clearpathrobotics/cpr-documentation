3fc8728bdafd06b05428e780b03cce58bde52df5
Rhys Faultless <rfaultless@clearpathrobotics.com>
2022-08-10 14:01:27 -0400

    removing unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import clsx from "clsx";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import HomepageMarkdown from "/src/pages/homepage.mdx";
 
