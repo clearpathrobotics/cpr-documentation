ddf52c4f57839edbdd94f6456877cb4b61ab154c
Jason Higgins <jhiggins@clearpathrobotics.com>
2023-02-14 16:32:41 -0500

    Added framework for OutdoorNav and IndoorNav user manuals

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,40 +1,73 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Clearpath Robotics Documentation',
-  url: 'https://docs.clearpathrobotics.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/website_images/favicon.ico',
+  title: "Clearpath Robotics Documentation",
+  url: "https://docs.clearpathrobotics.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/website_images/favicon.ico",
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-          showLastUpdateTime: true
-        },
+        docs: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
+    ],
+  ],
+
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        path: "docs",
+        routeBasePath: "docs",
+        sidebarPath: require.resolve("./sidebars.js"),
+        remarkPlugins: [math],
+        rehypePlugins: [katex],
+        showLastUpdateTime: true,
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "indoornav_user_manual",
+        path: "docs_indoornav_user_manual",
+        routeBasePath: "docs_indoornav_user_manual",
+        sidebarPath: require.resolve("./sidebars.js"),
+        remarkPlugins: [math],
+        rehypePlugins: [katex],
+        showLastUpdateTime: true,
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "outdoornav_user_manual",
+        path: "docs_outdoornav_user_manual",
+        routeBasePath: "docs_outdoornav_user_manual",
+        sidebarPath: require.resolve("./sidebars.js"),
+        remarkPlugins: [math],
+        rehypePlugins: [katex],
+        showLastUpdateTime: true,
+      },
     ],
   ],
 
@@ -42,98 +75,100 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: ' ',
+        title: " ",
         logo: {
-          alt: 'Clearpath Robotics',
-          src: 'img/website_images/logo_yellow.png',
-          srcDark: 'img/website_images/logo_white.png',
+          alt: "Clearpath Robotics",
+          src: "img/website_images/logo_yellow.png",
+          srcDark: "img/website_images/logo_white.png",
         },
         items: [
           {
-            to: '/docs/robots',
-            label: 'Robots',
-            position: 'left'
+            to: "/docs/robots",
+            label: "Robots",
+            position: "left",
           },
           {
-            to: '/docs/computers',
-            label: 'Computers',
-            position: 'left'
+            to: "/docs/computers",
+            label: "Computers",
+            position: "left",
           },
           {
-            to: '/docs/sensors',
-            label: 'Sensors',
-            position: 'left'
+            to: "/docs/sensors",
+            label: "Sensors",
+            position: "left",
           },
           {
-            to: '/docs/manipulators',
-            label: 'Manipulators',
-            position: 'left'
+            to: "/docs/manipulators",
+            label: "Manipulators",
+            position: "left",
           },
           {
-            to: '/docs/accessories',
-            label: 'Accessories',
-            position: 'left'
+            to: "/docs/accessories",
+            label: "Accessories",
+            position: "left",
           },
           {
-            to: '/docs/components',
-            label: 'Components',
-            position: 'left'
+            to: "/docs/components",
+            label: "Components",
+            position: "left",
           },
           {
-            to: '/docs/software',
-            label: 'Software',
-            position: 'left'
+            to: "/docs/software",
+            label: "Software",
+            position: "left",
           },
           {
-            to: '/docs/tools',
-            label: 'Tools',
-            position: 'left'
+            to: "/docs/tools",
+            label: "Tools",
+            position: "left",
           },
           {
-            to: 'about',
-            label: 'About',
-            position: 'right'
+            to: "about",
+            label: "About",
+            position: "right",
           },
           {
-            to: 'https://github.com/clearpathrobotics/cpr-documentation',
-            label: 'GitHub',
-            position: 'right',
+            to: "https://github.com/clearpathrobotics/cpr-documentation",
+            label: "GitHub",
+            position: "right",
           },
           {
-            to: 'https://store.clearpathrobotics.com/',
-            label: 'Store',
-            position: 'right'
+            to: "https://store.clearpathrobotics.com/",
+            label: "Store",
+            position: "right",
           },
           {
-            to: 'https://clearpathrobotics.com/',
-            label: 'Home',
-            position: 'right'
-          }
+            to: "https://clearpathrobotics.com/",
+            label: "Home",
+            position: "right",
+          },
+          // {
+          //   type: 'docsVersionDropdown',
+          //   docsPluginId: 'outdoornav_user_manual',
+          // }
         ],
       },
       docs: {
         sidebar: {
           autoCollapseCategories: true,
-          hideable: true
+          hideable: true,
         },
       },
       footer: {
-        copyright: `Copyright © 2022 Clearpath Robotics Inc. All rights reserved.`
+        copyright: `Copyright © 2022 Clearpath Robotics Inc. All rights reserved.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-      }
-    }
-  ),
-  
+      },
+    }),
+
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 };
