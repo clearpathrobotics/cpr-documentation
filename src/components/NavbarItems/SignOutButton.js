bd36334324b8972357a04dec33b6dcf7b63729fa
Rhys Faultless <rfaultless@clearpathrobotics.com>
2023-05-08 12:20:12 -0400

    Add console logging to SignOut button

--- src/components/NavbarItems/SignOutButton.js
+++ src/components/NavbarItems/SignOutButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Auth } from 'aws-amplify';
 
 async function signOut() {
+  console.log('SignOut button was pressed')
   try {
       await Auth.signOut();
   } catch (error) {
