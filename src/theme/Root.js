16152fee57f98fa57b5f468ca81c2d36af0bb396
Rhys Faultless <rfaultless@clearpathrobotics.com>
2023-05-08 13:16:48 -0400

    Removed withAuthenticator wrapper.
    Added FooterButton wth sign-in and sign-out behaviour.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../../aws-exports';
 Amplify.configure(awsExports);
 
 function Root({children}) {
+  const [signInRequested, changeSignInRequested] = useState(false);
+
+  async function signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+  }
+
+  function handleClick() {
+    changeSignInRequested(!signInRequested);
+    if (signInRequested) signOut();
+  }
+
+  function FooterButton() {
+    return (
+      <button onClick={handleClick}>{signInRequested ? 'Sign Out' : 'Sign In'}</button>
+    )
+  }
+  
   return (
     <>
       {children}
+      <FooterButton />
+      { signInRequested && <Authenticator variation="modal"></Authenticator> }
     </>
   );
 }
 
-export default withAuthenticator(Root);
+export default Root;
