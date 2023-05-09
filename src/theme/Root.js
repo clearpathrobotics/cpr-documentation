2804345ce773112550a8b5337fafe281b914c716
Rhys Faultless <rfaultless@clearpathrobotics.com>
2023-05-09 08:45:18 -0400

    Added function to check if user is signed in. Used function to update login button text.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
@@ -9,6 +9,7 @@ Amplify.configure(awsExports);
 
 function Root({children}) {
   const [signInRequested, changeSignInRequested] = useState(false);
+  const [isUserSignedIn, changeIsUserSignedIn] = useState(false);
 
   async function signOut() {
     try {
@@ -18,16 +19,37 @@ function Root({children}) {
     }
   }
 
+  async function checkIfSignedIn() {
+    try {
+        await Auth.currentAuthenticatedUser();
+        changeIsUserSignedIn(true);
+        return true;
+    } catch {
+        changeIsUserSignedIn(false);
+        return false;
+    }
+  }
+
   function handleClick() {
     changeSignInRequested(!signInRequested);
+    checkIfSignedIn();
     if (signInRequested) signOut();
   }
 
   function FooterButton() {
     return (
-      <button onClick={handleClick}>{signInRequested ? 'Sign Out' : 'Sign In'}</button>
+      <button onClick={handleClick}>{isUserSignedIn ? 'Sign Out' : 'Sign In'}</button>
     )
   }
+
+  // Checks if signed in using Auth, every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      checkIfSignedIn()
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [])
+  
   
   return (
     <>
