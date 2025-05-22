b42ea29f755061ef7377e9806bd284de5d1b65e4
Michael Hosmar <mikehosmar@gmail.com>
2025-05-22 19:43:59 +0000

    Better title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
           src="/img/website_images/logo_rockwell_pipe_clearpath_white.png"
           width={500}
         ></img>
-        <p className={styles.titleText}>Documentation and Tutorials for Robotics Development</p>
+        <p className={styles.titleText}>Mikes Awesome Edit!</p>
       </div>
     </header>
   );
